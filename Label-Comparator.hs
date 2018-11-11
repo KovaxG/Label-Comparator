df215797e03785224aad7a30b40d904330c310a5
KovaxG <georgesmth202@gmail.com>
2018-11-11 10:41:51 +0200

    Added some stuff
    And also made reporting nicer.

--- Label-Comparator.hs
+++ Label-Comparator.hs
@@ -11,18 +11,27 @@
 -}
 module Main where
 
-import Data.Either
+import Data.Either.Extra
 import Data.List
 import Debug.Trace
 import Text.Parsec
+import Text.Printf
 
 type Label = String
 data Result = Result {
     name :: String,
+    labelNr :: Int,
+    truePositive :: Int,
+    trueNegative :: Int,
+    falsePositive :: Int,
+    falseNegative :: Int,
     accuracy :: Double,
     precision :: Double,
     recall :: Double
-}  deriving (Show)
+}
+
+anomaly = "1" :: String
+nominal = "0" :: String
 
 readCSV :: String -> [[Label]]
 readCSV =
@@ -35,14 +44,34 @@ readCSV =
         line = sepBy cols (char ',')
         cols = many alphaNum
 
-toResult :: String -> (Double, Double, Double, Double) -> Result
-toResult name (tp, tn, fp, fn) =  
+toResult :: String -> Int -> (Double, Double, Double, Double) -> Result
+toResult name nr (tp, tn, fp, fn) =  
     Result {
         name = name,
+        labelNr = nr,
+        truePositive = round tp,
+        trueNegative = round tn,
+        falsePositive = round fp,
+        falseNegative = round fn,
         accuracy = (tp + tn) / (tp + tn + fp + fn),
         precision = tp / (tp + tn),
         recall = tp / (tp + fp) 
     } 
+
+instance Show Result where
+    show r = unlines [
+        name r ++ ": ",
+        "LabelNr: " ++ show (labelNr r),
+        "True Positive: " ++ show (truePositive r),
+        "True Negative: " ++ show (trueNegative r),
+        "False Positive: " ++ show (falsePositive r),
+        "False Negative: " ++ show (falseNegative r),
+        "Accuracy: " ++ printf doubleFormat (accuracy r),
+        "Precision: " ++ printf doubleFormat (precision r),
+        "Recall: " ++ printf doubleFormat (recall r)
+        ]
+        where doubleFormat = "%.4f"
+        
 
 compareMethods :: [[Label]] -> [Result]
 compareMethods ds = map evalMethod $ tail ds
@@ -51,16 +80,16 @@ compareMethods ds = map evalMethod $ tail ds
 
         evalMethod :: [Label] -> Result
         evalMethod (name:labels) = 
-            toResult name
+            toResult name (length labels)
             . foldl f (0, 0, 0, 0) 
             . zip baseTruth $ labels
 
         f (tp, tn, fp, fn) (t, p)
-            | t == "1" && p == "1" = (tp+1, tn, fp, fn)
-            | t == "0" && p == "0" = (tp, tn+1, fp, fn)
-            | t == "0" && p == "1" = (tp, tn, fp+1, fn)
-            | t == "1" && p == "0" = (tp, tn, fp, fn+1)
-         
+            | t == anomaly && p == anomaly = (tp+1, tn, fp, fn)
+            | t == nominal && p == nominal = (tp, tn+1, fp, fn)
+            | t == nominal && p == anomaly = (tp, tn, fp+1, fn)
+            | t == anomaly && p == nominal = (tp, tn, fp, fn+1)
+
 main :: IO ()
 main = do
     myData <- readCSV <$> readFile "test.csv"
